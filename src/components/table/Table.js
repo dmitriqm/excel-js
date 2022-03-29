8b55ca4251e0149f8a19fdf97f2a004c80ddcece
1dmitriqm@gmail.com <1dmitriqm@gmail.com>
2022-03-30 00:02:41 +0500

    Добавил шаблоны  верстки в компоненты

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,43 @@ export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   toHTML() {
-    return '<h1>Table</h1>'
+    return `
+      <div class="row">
+        <div class="row-info"></div>
+
+        <div class="row-data">
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+          <div class="column">A</div>
+          <div class="column">B</div>
+          <div class="column">C</div>
+        </div>
+      </div>
+    `
   }
 }
