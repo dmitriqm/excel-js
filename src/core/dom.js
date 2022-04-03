163b3aa2ee7479e33b2287dc08f530c5a2ebc6af
1dmitriqm@gmail.com <1dmitriqm@gmail.com>
2022-04-03 14:15:25 +0500

    resize column

--- src/core/dom.js
+++ src/core/dom.js
@@ -43,6 +43,14 @@ class Dom {
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
   }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
 }
 
 export function $(selector) {
